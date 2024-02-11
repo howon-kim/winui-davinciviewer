a60464b8f97b718363bfcc7f717b555c6f986612
Howon Kim <journal-02-permit@icloud.com>
2024-02-11 16:09:31 -0500

    finished first alpha

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,14 +5,18 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.Web.WebView2.Core;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.ApplicationSettings;
+using Windows.UI.WebUI;
 using Windows.Web.UI.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -20,40 +24,58 @@ using Windows.Web.UI.Interop;
 
 namespace DavinciViewer
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainWindow : Window
     {
+        private Dictionary<NavigationViewItem, Uri> loadedUris = new Dictionary<NavigationViewItem, Uri>();
+        private Dictionary<NavigationViewItem, WebView2> webviews = new Dictionary<NavigationViewItem, WebView2>();
+
         public MainWindow()
         {
             this.InitializeComponent();
+            InitializeWebView();
             NavView.SelectedItem = NavView.MenuItems[0];
-            NavigateToWebsite("https://www.google.com"); // Navigate to the initial website
         }
 
-        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        private async void InitializeWebView()
         {
-            if (args.IsSettingsSelected)
-            {
-                // Handle settings selection if needed
-            }
-            else
-            {
-                if (args.SelectedItem.GetType() == typeof(NavigationViewItem))
+            var selectedItem = NavView.MenuItems[0] as NavigationViewItem;
+            Debug.WriteLine("START!");
+            webviews[selectedItem] = new WebView2();
+            var uri = new Uri(selectedItem.Tag.ToString());
+            webviews[selectedItem].Source = uri;
+            await webviews[selectedItem].EnsureCoreWebView2Async();
+            await Task.Delay(TimeSpan.FromSeconds(60));
+
+            for (int i = 1; i < NavView.MenuItems.Count; i++) {
+           
+                if (NavView.MenuItems[i] is NavigationViewItem)
                 {
-                    var item = args.SelectedItem as NavigationViewItem;
-                    string websiteUrl = item.Tag.ToString();
-                    NavigateToWebsite(websiteUrl);
+                    selectedItem = NavView.MenuItems[i] as NavigationViewItem;
+                    Debug.WriteLine(i);
+                    webviews[selectedItem] = new WebView2();
+                    uri = new Uri(selectedItem.Tag.ToString());
+                    webviews[selectedItem].Source = uri;
+                    await webviews[selectedItem].EnsureCoreWebView2Async();
+                    webviews[selectedItem].CoreWebView2.NewWindowRequested += WebViewControl_New_NewWindowRequested;
                 }
             }
+            
+            // Ensure CoreWebView2 is ready
+            //await WebViewControl.EnsureCoreWebView2Async();
+
+            // Navigate to the initial website
+            //NavigateToWebsite("https://www.google.com");
         }
 
-        private void NavigateToWebsite(string url)
+        private async void NavigateToWebsite(string url)
         {
             try
             {
-                WebViewControl.Source = new Uri(url);
+                // Navigate to the URL
+                //WebViewControl.Source = new Uri(url);
+                //await WebViewControl.EnsureCoreWebView2Async();
+                //WebViewControl.CoreWebView2.NewWindowRequested += WebViewControl_New_NewWindowRequested;
             }
             catch (Exception ex)
             {
@@ -61,5 +83,40 @@ namespace DavinciViewer
                 Console.WriteLine($"Error navigating to website: {ex.Message}");
             }
         }
+
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            ContentGrid.Children.Clear();
+            // NEW CODE
+            var selectedItem = args.SelectedItem as NavigationViewItem;
+            if (!webviews.ContainsKey(selectedItem) )
+            {
+                Debug.WriteLine("Create new Webview2");
+                webviews[selectedItem] = new WebView2();
+                var uri = new Uri(selectedItem.Tag.ToString());
+                webviews[selectedItem].Source = uri;
+            } 
+
+            Debug.WriteLine("Loading the Website");
+            ContentGrid.Children.Add(webviews[selectedItem]);
+        }
+
+        private void WebViewControl_NavigationStarting(WebView2 sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            // Cancel the navigation to prevent it from opening in a new window
+            args.Cancel = true;
+
+            // Navigate to the URL using the same WebView control
+            NavigateToWebsite(args.Uri.ToString());
+        }
+
+        private void WebViewControl_New_NewWindowRequested(object sender,CoreWebView2NewWindowRequestedEventArgs args)
+        {
+            args.NewWindow = (CoreWebView2)sender;
+            //e.Handled = true;
+        }
+
+
     }
 }
